11e944ff4efba2c58e0ff7edd9693e80d45b2e8c
kimberlychapman <63875440+kimberlychapman@users.noreply.github.com>
2020-11-21 10:19:29 -0600

    added dropdown to sidebar

--- index2.js
+++ index2.js
@@ -2,6 +2,25 @@
 //     document.querySelector(".box").innerHTML=l;
 // };
 // document.querySelector('.box').onlcick=addTally;
+
+$('input[name="radio-btn"]').wrap('<div class="radio-btn"><i></i></div>');
+$(".radio-btn").on('click', function () {
+    var _this = $(this),
+        block = _this.parent().parent();
+    block.find('input:radio').attr('checked', false);
+    block.find(".radio-btn").removeClass('checkedRadio');
+    _this.addClass('checkedRadio');
+    _this.find('input:radio').attr('checked', true);
+});
+$('input[name="check-box"]').wrap('<div class="check-box"><i></i></div>');
+$.fn.toggleCheckbox = function () {
+    this.attr('checked', !this.attr('checked'));
+}
+$('.check-box').on('click', function () {
+    $(this).find(':checkbox').toggleCheckbox();
+    $(this).toggleClass('checkedBox');
+});
+
 function showImage(){
     document.getElementById('loadingImage').style.visibility=    document.getElementById('loadingImage').style.visibility == 'visible'? 'hidden' : 'visible';
 }
